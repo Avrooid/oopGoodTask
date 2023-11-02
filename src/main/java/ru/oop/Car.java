27c878f635748cd1f6016fb5ad0497c0cbd07934
Nikita <nikitaburakov08@gmail.com>
2023-11-02 15:59:25 +0500

    Сделал 2 и 3 задания

---
+++ src/main/java/ru/oop/Car.java
@@ -0,0 +1,22 @@
+package ru.oop;
+
+/**
+ * Класс машины
+ */
+public class Car implements Rideable{
+    /**
+     * Перемещает человека
+     */
+    @Override
+    public void move(Person person) {
+
+    }
+
+    /**
+     * Получает позицию машины
+     */
+    @Override
+    public Position getPosition() {
+        return null;
+    }
+}
